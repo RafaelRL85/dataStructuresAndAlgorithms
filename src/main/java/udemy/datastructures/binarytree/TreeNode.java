9036e72edbb9e3d3aecaf5e931ec11c0f93a884e
Rafael Ramirez Lozano <rapharl85@gmail.com>
2017-06-24 15:53:27 -0500

    some comments to try to explain how it works

--- src/main/java/udemy/datastructures/binarytree/TreeNode.java
+++ src/main/java/udemy/datastructures/binarytree/TreeNode.java
@@ -11,16 +11,24 @@ import java.util.List;
  */
 public class TreeNode {
 
+    //this is the element to be inserted
     private int root;
+    //this is just here to represent the left side of the node
     private TreeNode left;
+    //this is just here to represent the right side of the node
     private TreeNode right;
 
+    //with this constructor we send in the element to be inserted and it will create as null the two sides.
     public TreeNode(int root) {
         this.root = root;
     }
 
     /**
-     *
+     *this method accepts the new element and it will check
+     * if the new element is less thatn the original element
+     * if it is it will check against one or both sides for null
+     * if they aree null it will call the constructor to insert the new element
+     * if they are not, it will call itself recursively to check against more nodes.
      * @param newRoot
      */
     public void addNode(int newRoot) {
@@ -39,6 +47,7 @@ public class TreeNode {
         }
     }
 
+    //this will just return a list of ordered elements in ascending order
     public void preOrderTraversal(List<Integer> list){
         if (left != null) {
             left.preOrderTraversal(list);
@@ -49,6 +58,7 @@ public class TreeNode {
         }
     }
 
+    //this regurns a list of ordered elements descending order
     public void postOrderTraversal(List<Integer> list){
         if(right != null) {
             right.postOrderTraversal(list);
